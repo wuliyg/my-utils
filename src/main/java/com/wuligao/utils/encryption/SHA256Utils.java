d657ce65f2e65d9d4b5a81938df3b7ec19a7e409
wuligao <yuangao1111email@163.com>
2020-03-10 16:39:42 +0800

    第一次提交

---
+++ src/main/java/com/wuligao/utils/encryption/SHA256Utils.java
@@ -0,0 +1,52 @@
+package com.wuligao.utils.encryption;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Slf4j
+public class SHA256Utils {
+
+    /**
+     * 　　* 利用java原生的摘要实现SHA256加密
+     * 　　* @param str 加密后的报文
+     * 　　* @return
+     *
+     */
+    public static String getSHA256StrJava(String str) {
+        MessageDigest messageDigest;
+        String encodeStr = "";
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(str.getBytes("UTF-8"));
+            encodeStr = byte2Hex(messageDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            log.error("SHA-256 加密失败, 失败原因:{}", e.getMessage());
+        } catch (UnsupportedEncodingException e) {
+            log.error("SHA-256 加密失败, 失败原因:{}", e.getMessage());
+        }
+        return encodeStr;
+    }
+
+    /**
+     * 将byte转为16进制
+     * @param bytes
+     * @return
+     */
+    private static String byte2Hex(byte[] bytes) {
+        StringBuffer stringBuffer = new StringBuffer();
+        String temp = null;
+        for (int i = 0; i < bytes.length; i++) {
+            temp = Integer.toHexString(bytes[i] & 0xFF);
+            if (temp.length() == 1) {
+                //1得到一位的进行补0操作
+                stringBuffer.append("0");
+            }
+            stringBuffer.append(temp);
+        }
+        return stringBuffer.toString();
+    }
+
+}
