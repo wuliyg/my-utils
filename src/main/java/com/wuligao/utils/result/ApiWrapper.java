6e303fb904e270f8fc143bca845e05537260d364
wuligao <yuangao1111email@163.com>
2020-03-10 17:10:36 +0800

    引入接口返回封装类

---
+++ src/main/java/com/wuligao/utils/result/ApiWrapper.java
@@ -0,0 +1,151 @@
+package com.wuligao.utils.result;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+/**
+ * @author wuligao
+ */
+@Data
+public class ApiWrapper<T> implements Serializable {
+
+    /**
+     * 序列化标识
+     */
+    private static final long serialVersionUID = 4893280118017319089L;
+
+    /**
+     * 成功码.
+     */
+    public static final int SUCCESS_CODE = 20000;
+
+    /**
+     * 成功信息.
+     */
+    public static final String SUCCESS_MESSAGE = "操作成功";
+
+    /**
+     * 错误码.
+     */
+    public static final int ERROR_CODE = 50000;
+
+    /**
+     * 错误信息.
+     */
+    public static final String ERROR_MESSAGE = "内部异常";
+
+    /**
+     * 错误码：参数非法
+     */
+    public static final int ILLEGAL_ARGUMENT_CODE = 40000;
+
+    /**
+     * 错误信息：参数非法
+     */
+    public static final String ILLEGAL_ARGUMENT_MESSAGE = "参数非法";
+
+    /**
+     * 错误码：无权限
+     */
+    public static final int NOT_AUTH_CODE = 40001;
+
+    /**
+     * 错误信息：无权限
+     */
+    public static final String NOT_AUTH_MESSAGE = "权限验证错误";
+
+    /**
+     * 编号.
+     */
+    @ApiModelProperty("业务状态码")
+    private int result;
+
+    /**
+     * 信息.
+     */
+    @ApiModelProperty("业务信息")
+    private String resultInfo;
+
+    /**
+     * 结果数据
+     */
+    @ApiModelProperty("返回参数")
+    private T data;
+
+    public ApiWrapper() {
+    }
+
+    /**
+     * Instantiates a new wrapper.
+     *
+     * @param result     the result
+     * @param resultInfo the resultInfo
+     */
+    public ApiWrapper(int result, String resultInfo) {
+        this(result, resultInfo, null);
+    }
+
+    /**
+     * Instantiates a new wrapper.
+     *
+     * @param result     the result
+     * @param resultInfo the resultInfo
+     * @param data       the data
+     */
+    public ApiWrapper(int result, String resultInfo, T data) {
+        super();
+        this.result(result).resultInfo(resultInfo).data(data);
+    }
+
+    /**
+     * Sets the 编号 , 返回自身的引用.
+     *
+     * @param result the new 编号
+     * @return the wrapper
+     */
+    private ApiWrapper<T> result(int result) {
+        this.setResult(result);
+        return this;
+    }
+
+    /**
+     * Sets the 信息 , 返回自身的引用.
+     *
+     * @param resultInfo the new 信息
+     * @return the wrapper
+     */
+    private ApiWrapper<T> resultInfo(String resultInfo) {
+        this.setResultInfo(resultInfo);
+        return this;
+    }
+
+    /**
+     * Sets the 结果数据 , 返回自身的引用.
+     *
+     * @param data the new 结果数据
+     * @return the wrapper
+     */
+    public ApiWrapper<T> data(T data) {
+        this.setData(data);
+        return this;
+    }
+
+    /**
+     * 判断是否成功： 依据 Wrapper.SUCCESS_CODE == this.code
+     */
+    public boolean success() {
+        return ApiWrapper.SUCCESS_CODE == this.result;
+    }
+
+    /**
+     * 判断是否成功： 依据 Wrapper.SUCCESS_CODE != this.code
+     */
+    public boolean error() {
+        return !success();
+    }
+
+}
